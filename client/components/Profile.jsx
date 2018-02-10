0f8e323fb8e681b077db358963cb72f5ca9f0310
Starvon Washington <staronejazz@yahoo.com>
2018-02-10 13:51:49 -0800

    additional styles

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+// import CardExampleWithAvatar from './CardExampleWithAvatar.jsx'
 
 // user object passed
 // edit: boolean
@@ -12,7 +14,8 @@ class Profile extends Component {
 
   render() {
 
-    let connectOrEdit = (!this.props.myProfile) ? 'Connect' : 'Edit';
+    // let connectOrEdit = (!this.props.myProfile) ? 'Connect' : 'Edit';
+    let connectOrEdit = 'Connect';
     let user = this.props.user;
     let clickFun = this.props.clickFun;
 
@@ -33,28 +36,64 @@ class Profile extends Component {
       }}/>
     )
     submit = (this.props.edit) ?  submit : '';
-
+    var testing="bioInfo";
     return (
-      <div id="profile">
-        <input type="submit" value={connectOrEdit} onClick={() => {
-          clickFun(user);
-        }}/>
-        <div className="imgStyle">
-          <img src={this.props.user.image}/>
+      //       <div class="jumbotron">
+      //   <h1>Hello, world!</h1>
+      //   <p>...</p>
+      //   <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+      // </div>
+
+      // OPENING DIV
+      <div>
+      
+        <div className="masthead responsive-image">
+          <header className="entry-header">
+            {/* <h1 className="entry-title">Headding Spaced with padding</h1>
+            <h2 className="entry-subtitle">Some sort of lovely supporting title</h2> */}
+            </header>
         </div>
-        <div className="bio">
-          <h4>Bio</h4>
-          <p className="bioStyles"> {this.props.user.bio}</p>
-        </div>
-        <div className="skills">
-          <h4>Skills</h4>
-          <p className="skillsStyles">{this.props.user.skills}</p>
-        </div>
-        <div className="interests">
-          <h4>Interests</h4>
-          <p className="interestStyles">{this.props.user.interests}</p>
-        </div>
-        {submit}
+
+
+
+
+
+
+
+            <div id="profile">
+              <input type="submit" className="buttonConnect btn btn-lg" value={connectOrEdit} onClick={() => {
+                clickFun(user);
+              }}/>
+              <div className="imgStyle">
+                {/* <img src={this.props.user.image}/> */}
+                <img src="https://imgix.ranker.com/user_node_img/105/2097374/original/steve-jobs-people-in-film-photo-u10?w=650&q=50&fm=jpg&fit=crop&crop=faces" className="profile-Image"/>
+              </div>
+                <div id='userInfo'>
+                    <div id="bio">
+                      <h4>Bio</h4>
+                      <textarea className="bioStyles"  disable={(connectOrEdit === 'Edit') ? false : true }defaultValue={testing}/>
+                    </div>
+                    <div className="skills">
+                      <h4>Skills</h4>
+                      <textarea className="skillsStyle" disable={(connectOrEdit === 'Edit') ? false : true } defaultValue={testing}/>
+                    </div>
+                    <div className="interests">
+                      <h4>Interests</h4>
+                      <textarea className="interestStyle" disable={(connectOrEdit === 'Edit') ? false : true } defaultValue={testing}/>
+                    </div>
+                    <div className="languages">
+                     <img src="https://i.ytimg.com/vi/r8TKmjgKgB8/hqdefault.jpg"/>
+                     <br/>
+                     {/* <img src="https://cdn.goconqr.com/uploads/flash_card/image_question/9840931/desktop_05282061-3ae3-4bc1-871c-655c18375e68.png"/> */}
+                     <br/>
+                     <img src="/Users/admin/github/codesmith/axolotl/css/images/software.png"/>
+                     <img src="/Users/admin/github/codesmith/axolotl/css/images/software.png"/>
+                    </div>
+                </div>
+              {submit}
+            </div>
+
+     
       </div>
     );
   }
